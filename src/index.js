c0a606cfc1692d639b285838f55a7a912fe2fc2b
harsha <c.harsha0909@gmail.com>
2024-07-18 22:09:23 +0530

    Implemented email service from queue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const amqplib = require("amqplib");
+const { EmailService } = require("./services");
+
+async function connectQueue() {
+  try {
+    const connection = await amqplib.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+    const queue = "notifications";
+
+    await channel.assertQueue(queue);
+    channel.consume(queue, async (data) => {
+      console.log(`${Buffer.from(data.content)}`);
+      const object = JSON.parse(`${Buffer.from(data.content)}`);
+      await EmailService.sendMail(
+        object.recipientEmail,
+        object.subject,
+        object.text
+      );
+      channel.ack(data);
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 const { ServerConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
+const { log } = require("winston");
 
 const app = express();
 
@@ -11,5 +37,6 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+  await connectQueue();
   Logger.info("Successfully started the server");
 });
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,9 +1,15 @@
 const { TicketRepository } = require("../repositories");
 const { MAILER } = require("../config");
+const { ServerConfig } = require("../config");
 
 const ticketRepo = new TicketRepository();
 
-async function sendMail(mailFrom, mailTo, subject, text) {
+async function sendMail(
+  mailTo,
+  subject,
+  text,
+  mailFrom = ServerConfig.GMAIL_EMAIL
+) {
   try {
     const response = await MAILER.sendMail({
       from: mailFrom,
